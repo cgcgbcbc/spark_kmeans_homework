2db4aaac0cb051c855fc256283888921c9356a53
cgcgbcbc <cgcgbcbc@163.com>
2015-05-10 14:45:10 +0800

    add src directory

---
+++ src/main/java/cn/edu/tsinghua/Main.java
@@ -0,0 +1,10 @@
+package cn.edu.tsinghua;
+
+/**
+ * Created by guangchen on 5/10/15 14:13.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
